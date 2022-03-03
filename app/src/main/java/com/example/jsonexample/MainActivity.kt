f2d410b6bcfa9864ba087bd5a080292c3e62fba7
Simranjeet <simranjeet.banur@gmail.com>
2022-03-03 13:49:44 +0530

    new branch

--- app/src/main/java/com/example/jsonexample/MainActivity.kt
+++ app/src/main/java/com/example/jsonexample/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.jsonexample
 
+import android.annotation.SuppressLint
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
+import com.google.android.material.internal.ContextUtils.getActivity
 import org.json.JSONObject
 import java.io.InputStream
 import java.nio.charset.Charset
@@ -23,14 +25,16 @@ fun init() {
     val obj = JSONObject(this.loadJSONFromAsset())
     val userArray = obj.getJSONArray("users")
 
-    val userDetail = userArray.getJSONObject(2)
+    val userDetail = userArray.getJSONObject(0)
 users=userDetail.getString("1")
 
     text.setText(users)
 
 }
+@SuppressLint("RestrictedApi")
 fun Context.loadJSONFromAsset():String{
-    val t = assets.open("imp.json")
+//    val t = getActivity(this@MainActivity)!!.assets.openRawResourse("imp.json")
+val t = resources.openRawResource(R.raw.imp)
     val size = t.available()
     val buffer=ByteArray(size)
     t.read(buffer)
