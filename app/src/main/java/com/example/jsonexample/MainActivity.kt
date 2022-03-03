eaa93eaff1a4a728c1e7a29d638978c022db38de
amandeep <deep.khubbar@gmail.com>
2022-03-03 12:08:06 +0530

    JSonExample

---
+++ app/src/main/java/com/example/jsonexample/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.jsonexample
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import org.json.JSONObject
+import java.io.InputStream
+import java.nio.charset.Charset
+
+class MainActivity : AppCompatActivity() {
+    var users=""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+init()
+    }
+fun init() {
+    val text = findViewById<EditText>(R.id.name)
+    val obj = JSONObject(this.loadJSONFromAsset())
+    val userArray = obj.getJSONArray("users")
+
+    val userDetail = userArray.getJSONObject(2)
+users=userDetail.getString("1")
+
+    text.setText(users)
+
+}
+fun Context.loadJSONFromAsset():String{
+    val t = assets.open("imp.json")
+    val size = t.available()
+    val buffer=ByteArray(size)
+    t.read(buffer)
+    t.close()
+    return String(buffer, Charsets.UTF_8)
+}
+
+}
